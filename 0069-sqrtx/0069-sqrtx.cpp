635a96a2329190a2a6d339628ae55c8d833062a7
Kshitij Rastogi <rastogikshitij16@gmail.com>
2023-10-06 12:46:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (11.25%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return pow(x, 0.5);
+        int start = 1;
+        int end = x;
+        int res = 0;
+        
+        while(start <= end){
+            int mid = end + (start - end)/2;
+            if(mid <= x/mid){
+                start = mid + 1;
+                res = mid;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+    return res;
     }
-};
+}; 
