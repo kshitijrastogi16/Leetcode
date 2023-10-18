44c77140d68be8221cf6b766329cc36f8508f637
Kshitij Rastogi <rastogikshitij16@gmail.com>
2023-10-18 14:58:03 +0530

    Time: 742 ms (87.67%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,5 @@
 # Write your MySQL query statement below
-select w2.Id from Weather w1 inner join Weather w2 on w1.recordDate = DATE_ADD(w2.recordDate, interval -1 day) and w2.temperature > w1.temperature;
+select w2.Id 
+from Weather w1 
+inner join Weather w2 
+on w1.recordDate = DATE_ADD(w2.recordDate, interval -1 day) and w2.temperature > w1.temperature;
